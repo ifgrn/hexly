8acdd1cbcdee134569635ea4131266bc53f1dfa3
ifgrn <adbringas07@gmail.com>
2026-01-10 13:57:20 -0500

    ✨ feat: middleware

---
+++ server/src/middleware/middleware.ts
@@ -0,0 +1,26 @@
+import type { Context, Next } from "hono";
+import { getSignedCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
+import { COOKIE_NAME } from "../utils/cookie-options";
+
+export const middleware = async (c: Context, next: Next) => {
+  const secretKey = Bun.env.JWT_SECRET_KEY;
+
+  if (!secretKey) {
+    console.error("JWT_SECRET_KEY is not defined");
+    return c.json({ success: false, msg: "Internal Server Error" }, 500);
+  }
+
+  const cookie = await getSignedCookie(c, secretKey, COOKIE_NAME);
+
+  if (!cookie)
+    return c.json({ success: false, msg: "User not authorized" }, 401);
+
+  try {
+    const { id } = await verify(cookie, secretKey);
+    c.set("userId", id);
+    await next();
+  } catch (error) {
+    return c.json({ success: false, msg: "Invalid or expired token" }, 401);
+  }
+};
