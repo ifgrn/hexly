f8c0aad8bc66fb0d57377c3d5aaca24f1a171964
ifgrn <adbringas07@gmail.com>
2026-01-10 13:57:51 -0500

    🔧 chore: port and route config in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import usersRoute from "./routes/users";
+
+const app = new Hono();
+
+const PORT = Bun.env.PORT || 4000;
+app.use(logger());
+
+app.route("/api/auth", usersRoute);
+
+export default {
+  port: PORT,
+  fetch: app.fetch,
+};
