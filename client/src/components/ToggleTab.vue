b8a8c44702574b7de91a316d4e9931859353de9e
ifgrn <adbringas07@gmail.com>
2026-01-10 20:18:55 -0500

    ✨ feat: auth components and views

---
+++ client/src/components/ToggleTab.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+defineProps<{
+  modelValue: 'login' | 'register'
+}>()
+
+defineEmits<{
+  'update:modelValue': [value: 'login' | 'register']
+}>()
+</script>
+
+<template>
+  <div class="border-r border-t border-l w-fit flex rounded-none border-white/20">
+    <button
+      class="p-3 bg-[#0c0a09]"
+      :class="modelValue === 'login' ? 'bg-[#0c0a09] text-white font-medium' : 'opacity-20'"
+      @click="$emit('update:modelValue', 'login')"
+    >
+      Sign In
+    </button>
+    <button
+      class="p-3 bg-[#0c0a09]"
+      :class="modelValue === 'register' ? 'bg-[#0c0a09] text-white font-medium' : 'opacity-20'"
+      @click="$emit('update:modelValue', 'register')"
+    >
+      Sign Up
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/icons/SignOut.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    class="lucide lucide-log-out-icon lucide-log-out"
+    viewBox="0 0 24 24"
+  >
+    <path d="m16 17 5-5-5-5m5 5H9m0 9H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+  </svg>
+</template>
