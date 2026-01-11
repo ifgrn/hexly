0c7f34d051d80db01cac34cfdd4fdbe33101ec66
ifgrn <adbringas07@gmail.com>
2026-01-10 20:19:24 -0500

    ✨ feat: dashboard components and view

---
+++ client/src/components/HeaderLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="w-full border-b sticky top-0 z-50 border-white/20 mx-auto overflow-x-hidden">
+    <div class="flex justify-between md:mx-auto max-w-500">
+      <RouterLink to="/" class="flex items-center gap-1 w-fit p-4">
+        <HextechIcon class="size-7" />
+        <p class="text-sm">HEXTECH MANAGER</p>
+      </RouterLink>
+
+      <div v-if="router.path !== '/dashboard'" class="flex px-4 gap-4 md:hidden items-center">
+        <a target="_blank" rel="noopener noreferrer" :href="githubRepo">
+          <GithubIcon class="size-7" />
+        </a>
+        <a>
+          <MenuIcon class="size-7" />
+        </a>
+      </div>
+
+      <nav class="hidden md:flex" v-if="router.path !== '/dashboard'">
+        <RouterLink
+          to="/"
+          exact-active-class="after:w-full "
+          class="relative flex items-center justify-center px-7 border-white/20 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+        >
+          login.
+        </RouterLink>
+
+        <RouterLink
+          to="/dashboard"
+          exact-active-class="after:w-full "
+          class="relative flex items-center justify-center px-7 border-r border-white/20 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:bg-[#0c0a09]"
+        >
+          changelogs.
+        </RouterLink>
+        <a
+          :href="githubRepo"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="flex items-center justify-center px-4 bg-white/10 hover:bg-black"
+        >
+          <GithubIcon class="size-7" />
+        </a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { githubRepo } from '@/utils/const';
+import GithubIcon from './icons/GithubIcon.vue';
+import HextechIcon from './icons/HextechIcon.vue';
+import MenuIcon from './icons/MenuIcon.vue';
+import { RouterLink, useRoute } from 'vue-router';
+
+const router = useRoute();
+</script>
+
+<style scoped></style>
---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import AccountCard from '@/components/dashboard/AccountCard.vue';
+import AddAccountBtn from '@/components/dashboard/AddAccountBtn.vue';
+import UserMenu from '@/components/dashboard/UserMenu.vue';
+import { addAccountService } from '@/service/accounts-service';
+import { signoutHandler } from '@/service/auth-service';
+import { noir } from '@/utils/const';
+import { addAccountInputs } from '@/utils/form-consts';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const AccForm = ref<InstanceType<typeof AddAccountBtn> | null>(null);
+
+const signOutAction = async () => {
+  try {
+    const isLogOut = await signoutHandler();
+    if (isLogOut.success) router.push('/');
+    else return;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addAcctounAction = async (formData: FormData) => {
+  try {
+    const response = await addAccountService(formData);
+
+    if (response?.success) {
+      AccForm.value?.resetForm(); // 🔥 cierra modal
+      console.log(response);
+    }
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
+
+<template>
+  <section class="flex flex-col mx-auto mt-5 sm:mt-10 gap-4 w-100 sm:w-300">
+    <div class="flex justify-between">
+      <UserMenu username="noir" @signout="signOutAction" />
+      <AddAccountBtn ref="AccForm" :inputs="addAccountInputs" @submit="addAcctounAction" />
+    </div>
+    <div>
+      <AccountCard :account="noir" />
+    </div>
+  </section>
+</template>
+<style scoped></style>
