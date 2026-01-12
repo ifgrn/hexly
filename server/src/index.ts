4bd192d1045bdbeb4f5fc05f4496367f90503eb6
ifgrn <adbringas07@gmail.com>
2026-01-12 00:39:12 -0500

    CORS FIX 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,17 @@ import { logger } from "hono/logger";
 import usersRoute from "./routes/users";
 import { cors } from "hono/cors";
 
+export const config = {
+  runtime: "edge",
+};
+
 const app = new Hono();
 
 const PORT = Bun.env.PORT || 4000;
 app.use(logger());
 
 app.use("/api/*", cors({
-  origin: (origin) => origin,
+  origin: "http://localhost:5173",
   credentials: true,
   allowHeaders: ["Content-Type", "Authorization"],
   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -21,3 +25,4 @@ export default {
   port: PORT,
   fetch: app.fetch,
 };
+// 
